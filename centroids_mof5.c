bf5f36d9666ba266e23526da76372940415048d2
Armin Penz <Armin.Penz@student.uibk.ac.at>
2023-05-25 16:18:08 +0200

    First version of the programm.

--- centroids_mof5.c
+++ centroids_mof5.c
@@ -19,20 +19,28 @@ int main(void){
     int indexC5 = 50;
     int indexC6 = 56;
 
+    int j = 0;
 
 
+    for (int i = 0; i<=23; i++) {
+        printf("ar[%d][%d]=%d; " , i , j , indexC1);
+        printf("ar[%d][%d]=%d; " , i , j+1 , indexC2);
+        printf("ar[%d][%d]=%d; " , i , j+2 , indexC3);
+        printf("ar[%d][%d]=%d; " , i , j+3 , indexC4);
+        printf("ar[%d][%d]=%d; " , i , j+4 , indexC5);
+        printf("ar[%d][%d]=%d;\n" , i , j+5 , indexC6);
+
+        indexC1 = indexC1 +16;
+        indexC2 = indexC2 +16;
+        indexC3 = indexC3 +16;
+        indexC4 = indexC4 +16;
+        indexC5 = indexC5 +16;
+        indexC6 = indexC6 +16;
+
+    }
 
 
-
-
-
-
-
-
-
-
-
-    printf("Programm executed successfully!\n");
+    printf("Centroids generated successfully!\n");
 
     }
     else {

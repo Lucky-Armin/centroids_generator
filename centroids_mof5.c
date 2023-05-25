b83b6865aabb27e2b8fbb907571f0efb6740dccb
Armin Penz <Armin.Penz@student.uibk.ac.at>
2023-05-25 18:12:34 +0200

    Improvement of the code.

--- centroids_mof5.c
+++ centroids_mof5.c
@@ -7,50 +7,32 @@ int main(void){
 
 
     char *output_filename = "mof5-centroids.dat";
+    FILE * fileptr;
 
-    int check_name = access(output_filename, F_OK); 
-    if (check_name != 0) {
+    fileptr=fopen(output_filename, "r");
 
+     if(fileptr != NULL)
+     {
+       printf("File \"%s\" already exists!\n***********************\n******  EXITING  ******\n***********************\n" , output_filename);
+       return 1;
+     }
 
-    int indexC1 = 41;
-    int indexC2 = 43;
-    int indexC3 = 45;
-    int indexC4 = 48;
-    int indexC5 = 50;
-    int indexC6 = 56;
-
-    int j = 0;
+    FILE *output_file = fopen(output_filename, "w");      
+    int index[6] = {41, 43, 45, 48, 50, 56};
 
 
     for (int i = 0; i<=23; i++) {
-        printf("ar[%d][%d]=%d; " , i , j , indexC1);
-        printf("ar[%d][%d]=%d; " , i , j+1 , indexC2);
-        printf("ar[%d][%d]=%d; " , i , j+2 , indexC3);
-        printf("ar[%d][%d]=%d; " , i , j+3 , indexC4);
-        printf("ar[%d][%d]=%d; " , i , j+4 , indexC5);
-        printf("ar[%d][%d]=%d;\n" , i , j+5 , indexC6);
+        for (int j = 0; j<=5; j++) {
 
-        indexC1 = indexC1 +16;
-        indexC2 = indexC2 +16;
-        indexC3 = indexC3 +16;
-        indexC4 = indexC4 +16;
-        indexC5 = indexC5 +16;
-        indexC6 = indexC6 +16;
+        fprintf(output_file , "ar[%02d][%d]=%03d; " , i , j , index[j] + 16 * i);
 
+        }
+	
+	fprintf(output_file , "\n");
     }
 
-
+    fclose(output_file);
     printf("Centroids generated successfully!\n");
-
-    }
-    else {
-        printf("File \"%s\" already exists!\n***********************\n******  EXITING  ******\n***********************\n" , output_filename);
-        return 1;
-    }
-
-
-return 0;
+    return 0;
 }
-
-
 
